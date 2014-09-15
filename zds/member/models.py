c493a3f4afd59aca7926129d29e66c9709e958e5
firm1 <firm1@github.com>
2014-09-15 02:50:57 +0200

    modification du style d'affichage

--- zds/member/models.py
+++ zds/member/models.py
@@ -19,6 +19,7 @@ from zds.tutorial.models import Tutorial
 from zds.utils.models import Alert
 from django.utils.importlib import import_module
 
+
 class Profile(models.Model):
 
     """Represents an user profile."""
@@ -259,6 +260,7 @@ class TokenRegister(models.Model):
         """Textual forum of a profile."""
         return u"{0} - {1}".format(self.user.username, self.date_end)
 
+
 def save_profile(backend, user, response, *args, **kwargs):
     profile = Profile.objects.filter(user=user).first()
     if profile is None:
@@ -269,6 +271,7 @@ def save_profile(backend, user, response, *args, **kwargs):
                           email_for_answer=False)
         profile.last_ip_address = "0.0.0.0"
         profile.save()
+
 
 class Ban(models.Model):
 
