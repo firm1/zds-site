3844449502b4c3dd70b44edcdcd4c53bcdcea33b
Éfrit <8625408+VeryTastyTomato@users.noreply.github.com>
2020-04-04 00:20:56 +0200

    Format with vs-shell-format

--- scripts/ci_turbo.sh
+++ scripts/ci_turbo.sh
@@ -7,8 +7,7 @@ default_branch=dev
 git remote set-branches origin $default_branch
 git fetch --unshallow origin $default_branch
 
-if [[ ! -z "$TRAVIS_TAG" ]]
-then
+if [[ ! -z "$TRAVIS_TAG" ]]; then
     # Remember, we are sourced, we must not exit the main shell
     return
 fi
@@ -17,11 +16,9 @@ changed_files=$(git --no-pager diff --name-only "$TRAVIS_COMMIT" "$(git merge-ba
 echo "changed files:"
 echo "$changed_files"
 
-if ! echo "$changed_files" | grep -E -v "^assets"
-then
-    # Don't test the backend if only the `/assets/` directory changed
-    if [[ "$ZDS_TEST_JOB" == *"front"* ]]
-    then
+if ! echo "$changed_files" | grep -E -v "^assets"; then
+    # Don’t test the backend if only the `/assets/` directory changed
+    if [[ "$ZDS_TEST_JOB" == *"front"* ]]; then
         export ZDS_TEST_JOB="front"
     else
         export ZDS_TEST_JOB="none"
